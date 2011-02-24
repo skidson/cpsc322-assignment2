a6bbf8aeadb281cf4bd6b666504403f161d9022f
jeffpayan@gmail.com <jeffpayan@gmail.com@3ac47155-9346-b8be-38e7-8ca9d69f2b76>
2011-02-24 09:44:18 +0000

    completed

--- trunk/Assignment2/src/ca/ubc/cpsc322/sudoku/JeffSudokuSolver.java
+++ trunk/Assignment2/src/ca/ubc/cpsc322/sudoku/JeffSudokuSolver.java
@@ -3,6 +3,7 @@ package ca.ubc.cpsc322.sudoku;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -30,7 +31,7 @@ public class JeffSudokuSolver {
 		ArcConsistency(board, cells);
 		filledBoard = fillBoard(filledBoard, cells);
 
-		if(isComplete(board)) return filledBoard;
+		if(isComplete(filledBoard)) return filledBoard;
 
 		int[][] finalBoard = domainSplitSolve(filledBoard, cells);
 
@@ -38,72 +39,17 @@ public class JeffSudokuSolver {
 		if (finalBoard == null){
 			throw new Exception("No Solutions");
 		}
-		
+
 		return finalBoard;
 	}
 
-	private void init(int[][] board) {
-		//Initialize the cells and their domains
-		for (int x = 0; x < 9; x++)
-			for (int y = 0; y < 9; y++){
-				cells[x][y] = new Cell(board[x][y], x, y);
-				cells[x][y].initDomain();
-			}
-	}
-
-	/*public Cell[][] copyCells(Cell[][] origCells){
-		Cell[][] copy = new Cell[9][9];
-		for (int x = 0; x < 9; x++)
-			for (int y = 0; y < 9; y++) {
-				copy[x][y] = origCells[x][y].clone();
-			}
-		return copy;
-	}*/
-
-	public int[][] copyBoard(int[][] board){
-		int[][] copy = new int[9][9];
-		for (int x = 0 ; x < 9 ; x++) {
-			for(int y = 0 ; y < 9 ; y++) {
-				copy[x][y] = board[x][y];
-			}
-		}
-		return copy;
-	}
-
-	public boolean isComplete(int[][] board){
-		//Check to see if any domains have more than one value
-		if (board == null) return false;
-		
-		for(int x = 0 ; x < 9 ; x++) {
-			for (int y = 0 ; y < 9 ; y++) {
-				if (board[x][y] == 0) return false;
-			}
-		}
-		return true;
-	}
-
-	public int[][] fillBoard(int[][] board, Cell[][] cells) {
-		//If the cell has only one value in its domain, set its value to that remaining value in the domain
-		int[][] filledBoard = board;
-		for (int i = 0 ; i < 9 ; ++i) {
-			for (int j = 0 ; j < 9 ; ++j) {
-				if (cells[i][j].getDomain().size() == 1) {
-					cells[i][j].value = cells[i][j].domains.iterator().next();
-					filledBoard[i][j] = cells[i][j].value;
-				}
-			}
-		}
-		return filledBoard;
-	}
-
-
-	//Arc Consistency
+	//Make all arcs in the given board Arc Consistent
 	public void ArcConsistency(int[][] board, Cell[][] cells){
-		List<Arc> TDA = new ArrayList<Arc>();
+		List<Arc> TDA = new LinkedList<Arc>();
 
 		for (int x = 0; x < 9; x++)
 			for (int y = 0; y < 9; y++)
-				createArcs(TDA, x ,y);
+				createArcs(cells, TDA, x ,y);
 
 		while(TDA.size() != 0) {
 			//Remove an arc from TDA and get the cells that it is attached to
@@ -126,6 +72,7 @@ public class JeffSudokuSolver {
 						int j = domItr.next();
 						if (i != j) {
 							possible.add(i);
+
 							break;
 						}
 					}
@@ -139,109 +86,14 @@ public class JeffSudokuSolver {
 			//by changing its domain, and then set its domain to the possible
 			//set that was generated
 			if(!possible.equals(fromDomain)){
-				addModifiedDomainsToTDA(TDA, from.coord[0], from.coord[1]);
-				cells[from.coord[0]][from.coord[1]].domains = possible;					
+				addModifiedDomainsToTDA(cells, TDA, from.coord[0], from.coord[1]);
+				cells[from.coord[0]][from.coord[1]].domains = possible;	
 			}
 		}
 	}
-
-	public int[][] domainSplitSolve(int[][] board, Cell[][] cells) throws Exception{
-		if (isComplete(board)) {
-			return board;
-		}
-		//Make 2 deep copies of cells arrays
-		Cell[][] cells1 = cells.clone();
-		Cell[][] cells2 = cells.clone();
-		
-		splitDomains(cells1, cells2);
-		
-		//Make 2 deep copies of the board
-		int[][] copyboard1 = copyBoard(board);
-		int[][] copyboard2 = copyBoard(board);
-
-		fillBoard(copyboard1, cells1);
-		ArcConsistency(copyboard1, cells1);
-		fillBoard(copyboard2, cells2);
-		ArcConsistency(copyboard2, cells2);
-
-		if (checkDomains(cells1)) {
-			System.out.println("Going deeper");
-			copyboard1 = domainSplitSolve(copyboard1, cells1);
-		}
-		else copyboard1 = null;
-
-		if(checkDomains(cells2)) {
-			System.out.println("Going deeper");
-			copyboard2 = domainSplitSolve(copyboard2, cells2);
-		}
-		else copyboard2 = null;
-
-		if (isComplete(copyboard1) && isComplete(copyboard2) && !areBoardsEqual(copyboard1, copyboard2)) {
-			throw new Exception("Many solutions.");
-		}
-
-		if (isComplete(copyboard1)) return copyboard1;
-
-		if (isComplete(copyboard2)) return copyboard2;
-
-		return null;
-	}
 	
-	public void splitDomains(Cell[][] cells1, Cell[][] cells2){
-		for ( int x = 0 ; x < 9 ; x++ ) {
-			for ( int y = 0 ; y < 9 ; y++ ) {
-				if ( cells1[x][y].domains.size() > 1 ) {
-					int size = cells1[x][y].domains.size();
-					ArrayList<Integer> dom1nums = new ArrayList<Integer>(5);
-					ArrayList<Integer> dom2nums = new ArrayList<Integer>(5);
-					Iterator<Integer> itr = cells1[x][y].domains.iterator();
-
-					for (int k = 0 ; k < size ; k++) {
-						if (k <= (size-2)/2) {
-							dom1nums.add(itr.next());
-						} else {
-							dom2nums.add(itr.next());
-						}
-					}
-					
-					for(Integer k : dom1nums) cells1[x][y].domains.remove(k);
-					for(Integer k : dom2nums) cells2[x][y].domains.remove(k);
-					
-					return;
-				}
-			}
-		}
-		
-	}
-	
-	public boolean areBoardsEqual( int[][] board1, int[][] board2) {
-		if (board1 == null || board2 == null){
-			if (board1 == null && board2 == null) return true;
-			return false;
-		}
-		
-		for(int i = 0 ; i < 9 ; ++i)
-			for (int j = 0 ; j < 9 ; ++j)
-				if (board1[i][j] != board2[i][j]) return false;
-		return true;
-	}
-	
-	/**
-	 * Ensure every domain has at least one value
-	 * @param checkCells the cell array
-	 * @return true/false
-	 */
-	public boolean checkDomains(Cell[][] checkCells) {
-		for (int x = 0 ; x < 9 ; x++)
-			for (int y = 0 ; y < 9 ; y++)
-				if (checkCells[x][y].domains.size() == 0) return false;
-		return true;
-	}
-	
-	
-	public void createArcs(List<Arc> TDA, int x, int y){
+	public void createArcs(Cell[][] cells, List<Arc> TDA, int x, int y){
 		// Add all cells from this cell's row
-
 		for (int i = 0; i < 9; i++)
 			if (i != y ){
 				TDA.add(new Arc(cells[x][y], cells[x][i]));
@@ -261,8 +113,9 @@ public class JeffSudokuSolver {
 			}
 		}
 	}
-
-	public void addModifiedDomainsToTDA(List<Arc> TDA, int x, int y){
+	
+	//Revisit cells who's arcs may no longer be consistent due to the change of a domain on a connected cell
+	public void addModifiedDomainsToTDA(Cell[][] cells, List<Arc> TDA, int x, int y){
 		// Add all cells from this cell's row
 		for (int i = 0; i < 9; i++)
 			if (i != y ){
@@ -283,10 +136,166 @@ public class JeffSudokuSolver {
 			}
 		}
 	}
-	/* ************************ EMBEDDED CLASSES ************************ */
+	
+	public int[][] domainSplitSolve(int[][] board, Cell[][] cells) throws Exception{
+		if (isComplete(board)) {
+			return board;
+		}
+		//Make 2 deep copies of cells arrays
+		Cell[][] temp1 = copyCells(cells);
+		Cell[][] temp2 = copyCells(cells);
 
-	public class Cell {
-		Set<Integer> domains = new HashSet<Integer>();
+		splitDomains(temp1, temp2);
+
+
+		//Make 2 deep copies of the board
+		int[][] copyboard1 = copyBoard(board);
+		int[][] copyboard2 = copyBoard(board);
+
+		//Fill the boards with any possible values and then run arc consistency on the
+		//boards with split domains
+		fillBoard(copyboard1, temp1);
+		ArcConsistency(copyboard1, temp1);
+		fillBoard(copyboard2, temp2);
+		ArcConsistency(copyboard2, temp2);
+		
+		//If all the domains in the first split are valid, that is have at least one valid value
+		//split another domain
+		if (checkDomains(temp1)) {
+			copyboard1 = domainSplitSolve(copyboard1, temp1);
+		} else copyboard1 = null;
+
+		if(checkDomains(temp2)) {
+			copyboard2 = domainSplitSolve(copyboard2, temp2);
+		} else copyboard2 = null;
+		
+		
+		//Bonus question : If both boards are completely solved and they are equal then there are
+		//at least two solutions, throw exception
+		if (isComplete(copyboard1) && isComplete(copyboard2) && !areBoardsEqual(copyboard1, copyboard2)) {
+			throw new Exception("Many solutions.");
+		}
+		
+		//if either board is complete, return it otherwise return null
+		if (isComplete(copyboard1)) return copyboard1;
+
+		if (isComplete(copyboard2)) return copyboard2;
+
+		return null;
+	}
+
+	//Find the first domain with more than one value and split it in half
+	public void splitDomains(Cell[][] temp1, Cell[][] temp2){
+		for ( int x = 0 ; x < 9 ; x++ ) {
+			for ( int y = 0 ; y < 9 ; y++ ) {
+				if ( temp1[x][y].domains.size() > 1 ) {
+					int size = temp1[x][y].domains.size();
+					List<Integer> firstHalf = new ArrayList<Integer>(5);
+					List<Integer> secondHalf = new ArrayList<Integer>(5);
+					Iterator<Integer> itr = temp1[x][y].domains.iterator();
+					//Split the domain
+					for (int k = 0 ; k < size ; k++) {
+						if (k <= (size-2)/2) {
+							firstHalf.add(itr.next());
+						} else {
+							secondHalf.add(itr.next());
+						}
+					}
+					//Remove the values from original domains
+					for(Integer k : firstHalf) temp2[x][y].domains.remove(k);
+					for(Integer k : secondHalf) temp1[x][y].domains.remove(k);
+
+					return;
+				}
+			}
+		}
+	}
+	
+	/* **************************************** HELPER FUNCTIONS************************************ */
+	private void init(int[][] board) {
+		//Initialize the cells and their domains
+		for (int x = 0; x < 9; x++)
+			for (int y = 0; y < 9; y++){
+				cells[x][y] = new Cell(board[x][y], x, y);
+				cells[x][y].initDomain();
+			}
+	}
+
+	private Cell[][] copyCells(Cell[][] origCells){
+		Cell[][] copy = new Cell[9][9];
+		for (int x = 0; x < 9; x++)
+			for (int y = 0; y < 9; y++) {
+				copy[x][y] = origCells[x][y].clone();
+			}
+		return copy;
+	}
+	
+	//Make a deep copy of the board
+	private int[][] copyBoard(int[][] board){
+		int[][] copy = new int[9][9];
+
+		for (int x = 0 ; x < 9 ; x++) {
+			for(int y = 0 ; y < 9 ; y++) {
+				copy[x][y] = board[x][y];
+			}
+		}
+		return copy;
+	}
+	//Check if the board is complete (False if any value is still 0 (Unsolved))
+	private boolean isComplete(int[][] board){
+		if (board == null) return false;
+
+		for(int x = 0 ; x < 9 ; x++) {
+			for (int y = 0 ; y < 9 ; y++) {
+				if (board[x][y] == 0) return false;
+			}
+		}
+		return true;
+	}
+
+	//Fill the board will all known values ( only one value in cell's domain)
+	public int[][] fillBoard(int[][] board, Cell[][] cells) {	
+		int[][] filledBoard = board;
+		for (int i = 0 ; i < 9 ; ++i) {
+			for (int j = 0 ; j < 9 ; ++j) {
+				if (cells[i][j].getDomain().size() == 1) {
+					filledBoard[i][j] = cells[i][j].domains.iterator().next();
+				}
+			}
+		}
+		return filledBoard;
+	}
+	
+	//Check if two boards are identical
+	public boolean areBoardsEqual( int[][] board1, int[][] board2) {
+		if (board1 == null || board2 == null){
+			if (board1 == null && board2 == null) return true;
+			return false;
+		}
+
+		for(int i = 0 ; i < 9 ; ++i)
+			for (int j = 0 ; j < 9 ; ++j)
+				if (board1[i][j] != board2[i][j]) return false;
+		return true;
+	}
+
+	/**
+	 * Ensure every domain has at least one value
+	 * @param checkCells the cell array
+	 * @return true/false
+	 */
+	public boolean checkDomains(Cell[][] checkCells) {
+		for (int x = 0 ; x < 9 ; x++)
+			for (int y = 0 ; y < 9 ; y++)
+				if (checkCells[x][y].domains.size() == 0) return false;
+		return true;
+	}
+
+	/* ************************ EMBEDDED CLASSES ************************ */
+	
+	//A class to store information about every Cell on the sudoku board
+	private class Cell {
+		Set<Integer> domains;
 		int value;
 		int[] coord = new int[2];
 
@@ -295,8 +304,11 @@ public class JeffSudokuSolver {
 			coord[0] = x;
 			coord[1] = y;
 		}
-
+		
+		//Initialize the domain to (1-9) if the value is 0
+		//Otherwise set domain = value;
 		public void initDomain(){
+			domains = new HashSet<Integer>();
 			if (this.value == 0){
 				for(int i = 1; i < 10; i++)
 					domains.add(i);
@@ -306,20 +318,19 @@ public class JeffSudokuSolver {
 		public Cell clone() {
 			Cell theClone = new Cell(this.value, this.coord[0], this.coord[1]);
 			theClone.domains = new HashSet<Integer>();
-			for(Integer i : this.domains) theClone.domains.add(i);
+			Iterator<Integer> itr = this.domains.iterator();
+			while(itr.hasNext()){
+				theClone.domains.add(itr.next());
+			}
 			return theClone;
 		}
-		/**
-		 * Removes the specified value from this cell's domain. Returns true if this results in the cell
-		 * being complete, false otherwise.
-		 * @param value
-		 * @return A boolean indicating if this cell is now solved.
-		 */
+		
 		public Set<Integer> getDomain() {
 			return domains;
 		}			
 	}
-
+	
+	//A class to store arcs
 	private class Arc {
 		Cell fromNode;
 		Cell toNode;
